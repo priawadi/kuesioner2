9060c60bf332ef6cabb447791b10ac852c61705f
Ozi Priawadi <jie.ilkom46ipb@gmail.com>
2016-05-01 23:17:17 +0700

    Form Edit Rasa Percaya

---
+++ resources/views/rasa_percaya_masy/edit.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">{{ $subtitle }}</div>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+                <div class="panel-body">
+                    {{ Form::open(array('url' => $action, 'method' => 'patch')) }}
+                        <table class="table">
+                        @foreach ($pertanyaan as $idx_pertanyaan => $item)
+                        <tr>
+                            <td>{{$item->id_rasa_percaya}}.</td>
+                            <td>
+                                {{$item->pertanyaan_rasa_percaya}}
+                                @if ($item->is_reason)
+                                <br>
+                                Alasan:                             
+                                {{
+                                    Form::textarea(
+                                        'alasan[' . $jwb_rasa_percaya[$item->id_rasa_percaya]['id_jwb_rasa_percaya'] . ']', 
+                                        $jwb_rasa_percaya[$item->id_rasa_percaya]['jwb_teks_rasa_percaya'], 
+                                        [
+                                            'class'       => 'form-control col-sm-6',
+                                            'placeholder' => 'Alasan',
+                                            'rows'        => 4
+                                        ]
+                                    )
+                                }} 
+                                @endif
+                            </td>
+                            <td>
+                                @foreach ($opsi[$item->id_master_opsional] as $id_opsional => $opsional)
+                                <div class="radio">
+                                    <label>
+                                        {{
+                                            Form::radio(
+                                                'jawaban[' . $jwb_rasa_percaya[$item->id_rasa_percaya]['id_jwb_rasa_percaya'] . ']',
+                                                $id_opsional,
+                                                $id_opsional == $jwb_rasa_percaya[$item->id_rasa_percaya]['id_master_opsional'],
+                                                [
+                                                    'class' => 'control-label'
+                                                ]
+                                            )
+                                        }} 
+                                        {{$opsional}}
+                                    </label>
+                                </div>
+                                @endforeach
+                            </td>
+                        </tr>
+                        @endforeach
+                        </table>
+                        @include('components.form.prev_next_btn')
+                    {{ Form::close() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
