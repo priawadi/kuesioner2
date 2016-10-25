de7eff893726b3a283dd026f8c14afc111f090ec
Ozi Priawadi <jie.ilkom46ipb@gmail.com>
2016-10-25 16:00:43 +0700

    Add location in table

--- app/Http/Controllers/ExportKuesionerController.php
+++ app/Http/Controllers/ExportKuesionerController.php
@@ -237,6 +237,7 @@ class ExportKuesionerController extends Controller
             'Provinsi',
             'Status Responden',
             'Pengalaman Usaha',
+            'Lokasi',
             'Nama Enumerator',
             'Tanggal Wawancara',
             'Tanggal Editing',
@@ -733,6 +734,19 @@ class ExportKuesionerController extends Controller
             3 => 'ABK',
         ];
 
+        $lokasi_responden = [
+            1 => 'Batam',
+            2 => 'Sibolga',
+            3 => 'Langkat',
+            4 => 'Indramayu',
+            5 => 'Pangkajene Kepulauan',
+            6 => 'Bitung',
+            7 => 'Sorong',
+            8 => 'Merauke',
+            9 => 'Maluku Tengah',
+            10 => 'Cilacap',
+        ];
+
         $responden  = Responden::find($id_responden);
         $enumerator = Enumerator::where('id_responden', $id_responden)->first();
         return [
@@ -747,6 +761,7 @@ class ExportKuesionerController extends Controller
             $responden->provinsi,
             isset($status_responden[$responden->stat_responden])? $status_responden[$responden->stat_responden]: null,
             $responden->pengalaman_usaha,
+            isset($lokasi_responden[$responden->lokasi])? $lokasi_responden[$responden->lokasi]: null,
             $enumerator['nama_enumerator'],
             $enumerator['tanggal_wawancara'],
             $enumerator['tanggal_editing'],
